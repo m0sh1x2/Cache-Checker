1a57822190291016e3fb061fceca73ab61f8cfa6
Krasimir <krasimirvelichkov@gmail.com>
2019-07-27 13:54:05 +0300

    Initial Commit with basic structure

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SiteStatus :sites="sites" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SiteStatus from "./components/SiteStatus";
+import { sites } from "./data/sites.js";
 
 export default {
-  name: 'app',
+  name: "app",
+  data() {
+    return {
+      sites
+    };
+  },
   components: {
-    HelloWorld
+    SiteStatus: SiteStatus
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/data/sites.js
@@ -0,0 +1,5 @@
+export const sites = [
+  { id: 0, url: "https://news.ycombinator.com", status: "200" },
+  { id: 1, url: "https://lobste.rs/", status: "200" },
+  { id: 2, url: "https://medium.com/", status: "200" }
+];
