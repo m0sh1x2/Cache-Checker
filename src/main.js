d5de97c34b1203ba1c862fed097bf6fb4d5e9cda
Krasimir Velichkov <krasimirvelichkov@gmail.com>
2019-08-05 11:00:50 +0300

    Adding Vuetify

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 import VueRouter from 'vue-router';
+import Vuetify from 'vuetify/lib';
 import {
   routes
 } from '@/routes';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
+Vue.use(Vuetify);
 Vue.use(VueRouter);
 
 const router = new VueRouter({
